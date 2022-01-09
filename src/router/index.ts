02a83a76468b4c2d71697b0eefe14db24390c914
torish14 <nanamin.104@icloud.com>
2022-01-09 20:14:28 +0900

    ✨ feat: Todos.vue をルーティングに追加

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,8 +3,15 @@ import {
   createWebHistory,
   RouteRecordRaw,
 } from 'vue-router'
+import Todos from '@/views/Todos.vue'
 
-const routes: Array<RouteRecordRaw> = []
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Todos',
+    component: Todos,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),

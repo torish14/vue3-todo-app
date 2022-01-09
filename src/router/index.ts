bb19c8b6e6e54af693fba3463dac830d6413db8b
torish14 <nanamin.104@icloud.com>
2022-01-09 20:15:33 +0900

    ✨ feat: AddTodo.vue をルーティングに追加

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,18 @@ import {
   RouteRecordRaw,
 } from 'vue-router'
 import Todos from '@/views/Todos.vue'
+import AddTodo from '@/views/AddTodo.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Todos',
     component: Todos,
+  },
+  {
+    path: '/new',
+    name: 'AddTodo',
+    component: AddTodo,
   },
 ]
 

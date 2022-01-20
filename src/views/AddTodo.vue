fc6f6213c1fd82ea893510f9404206a4d7fa5507
torish14 <nanamin.104@icloud.com>
2022-01-20 14:46:03 +0900

    ✨ feat: Todo追加ページを作成

---
+++ src/views/AddTodo.vue
@@ -0,0 +1,67 @@
+<template>
+	<h2>Todo を作成する</h2>
+	<!-- フォームがサブミットされたときに onSubmit を呼び出す -->
+	<form @submit.prevent="onSubmit">
+		<div>
+			<label for="title">タイトル</label>
+			<!-- v-model でバインディング -->
+			<input type="text" id="title" v-model="data.title">
+		</div>
+		<div>
+			<label for="description">説明</label>
+			<!-- v-model でバインディング -->
+			<textarea id="description" v-model="data.description"></textarea>
+		</div>
+		<div>
+			<label for="status">ステータス</label>
+			<!-- v-model でバインディング -->
+			<select id="status" v-model="data.status">
+				<option value="waiting">waiting</option>
+				<option value="working">working</option>
+				<option value="completed">completed</option>
+				<option value="pending">pending</option>
+			</select>
+		</div>
+		<!-- フォームがサブミットされたときに onSubmit を呼び出す -->
+		<button @click="onSubmit">作成する</button>
+	</form>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject, reactive } from 'vue'
+import { useRouter } from 'vue-router'
+import { Params } from '@/store/todo/types'
+import { todoKey } from '@/store/todo'
+
+export default defineComponent({
+	setup() {
+		// ストアを inject
+		const todoStore = inject(todoKey)
+		if (!todoStore) {
+			throw new Error('todoStore is not found')
+		}
+
+		// router を利用するため､useRouter を呼び出す
+		const router = useRouter()
+
+		// データを reactive にする
+		const data = reactive<Params>({
+			title: '',
+			description: '',
+			status: 'waiting'
+		})
+
+		// フォームがサブミットされたときに onSubmit を呼び出す
+		const onSubmit = () => {
+			const { title,  description, status } = data
+			todoStore.addTodo({ title, description, status })
+			router.push('/')
+		}
+
+		return {
+			data,
+			onSubmit
+		}
+	}
+})
+</script>

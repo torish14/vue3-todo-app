650e490720ef1394a6505d10fa6c45483a7d0b2c
torish14 <nanamin.104@icloud.com>
2022-01-21 02:32:25 +0900

    ✨ feat: TodoItem コンポーネントをインポート

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -4,9 +4,15 @@
 			Todo 一覧です
 		</h2>
 		<ul>
-			<li v-for="todo in todoStore.state.todos" :key="todo.id">
+			<TodoItem
+				v-for="todo in todoStore.state.todos"
+				:key="todo.id"
+				:todo="todo"
+				@click-title="clickTitle"
+				@click-delete="clickDelete"
+			>
 				{{ todo.title }}
-			</li>
+			</TodoItem>
 		</ul>
 		<router-link to="/new">
 			新規作成
@@ -16,17 +22,36 @@
 
 <script lang="ts">
 import { defineComponent, inject } from 'vue'
+import { useRouter } from 'vue-router'
+import TodoItem from '@/components/TodoItem.vue'
 import { todoKey } from '../store/todo'
 
 export default defineComponent({
+	components: {
+		TodoItem
+	},
 	setup() {
 		const todoStore = inject(todoKey)
 		if (!todoStore) {
 			throw new Error('todoStore is not found')
 		}
 
+		const router = useRouter()
+
+		// 引数として受け取った todo の id を元に todoStore の todo を更新する
+		const clickDelete = (id: number) => {
+			todoStore.deleteTodo(id)
+		}
+
+		// 引数として受け取った todo の id を元に編集ページに遷移する
+		const clickTitle = (id: number) => {
+			router.push(`/edit/${id}`)
+		}
+
 		return {
-			todoStore
+			todoStore,
+			clickDelete,
+			clickTitle
 		}
 	}
 })

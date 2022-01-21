3c9a01e3d9c8cc917b82c0848ecccc15b0eaf1fc
torish14 <nanamin.104@icloud.com>
2022-01-22 06:50:24 +0900

    🐞 fix: モックデータを削除､型定義を修正

--- src/store/todo/index.ts
+++ src/store/todo/index.ts
@@ -5,99 +5,58 @@ import {
   TodoStore,
   Params,
 } from '@/store/todo/types'
-
-// 永続化していないため､とりあえずモックデータを用意
-const mockTodo: Todo[] = [
-  {
-    id: 1,
-    title: 'todo1',
-    description: '1つ目の Todo',
-    status: 'waiting',
-    createdAt: new Date('2022-01-09'),
-    updatedAt: new Date('2022-01-09'),
-  },
-  {
-    id: 2,
-    title: 'todo2',
-    description: '2つ目の Todo',
-    status: 'waiting',
-    createdAt: new Date('2022-01-10'),
-    updatedAt: new Date('2022-01-10'),
-  },
-  {
-    id: 3,
-    title: 'todo3',
-    description: '3つ目の Todo',
-    status: 'working',
-    createdAt: new Date('2022-01-11'),
-    updatedAt: new Date('2022-01-11'),
-  },
-  {
-    id: 4,
-    title: 'todo4',
-    description: '4つ目の Todo',
-    status: 'waiting',
-    createdAt: new Date('2022-01-12'),
-    updatedAt: new Date('2022-01-12'),
-  },
-  {
-    id: 5,
-    title: 'todo5',
-    description: '5つ目の Todo',
-    status: 'working',
-    createdAt: new Date('2022-01-13'),
-    updatedAt: new Date('2022-01-13'),
-  },
-]
+import Repository, { TODOS } from '@/clients/RepositoryFactory'
+const TodoRepository = Repository[TODOS]
 
 // reactive は､リアクティブデータを宣言する
 const state = reactive<TodoState>({
-  todos: mockTodo,
+  todos: [],
 })
 
-// 新たに作成された Todo の初期化処理
-const initializeTodo = (todo: Params) => {
-  const date = new Date()
-  return {
-    id: date.getTime(),
-    title: todo.title,
-    description: todo.description,
-    status: todo.status,
-    createdAt: date,
-    updatedAt: date,
-  } as Todo
+const fetchTodos = async (): Promise<void> => {
+  state.todos = await TodoRepository.getAll()
+}
+
+const fetchTodo = async (id: number): Promise<void> => {
+  const todo = await TodoRepository.get(id)
+  state.todos.push(todo)
 }
 
 // id を指定して Todo を取得する
-const getTodo = (id: number) => {
+const getTodo = (id: number): Todo => {
   const todo = state.todos.find((todo) => todo.id === id)
   if (!todo) {
-    throw new Error('Todo not found')
+    throw new Error(`cannot find todo by id:${id}`)
   }
   return todo
 }
 
 // 新たに Todo を追加する
-const addTodo = (todo: Params) => {
-  state.todos.push(initializeTodo(todo))
+const addTodo = async (todo: Params): Promise<void> => {
+  const result = await TodoRepository.create(todo)
+  state.todos.push(result)
 }
 
 // id を指定して Todo を更新する
-const updateTodo = (id: number, todo: Todo) => {
-  const index = state.todos.findIndex((t) => t.id === id)
+const updateTodo = async (id: number, todo: Todo): Promise<void> => {
+  const result = await TodoRepository.update(id, todo)
+  const index = state.todos.findIndex((todo) => todo.id === id)
   if (index === -1) {
-    throw new Error('Todo not found')
+    throw new Error(`cannot find todo by id:${id}`)
   }
-  state.todos[index] = todo
+  state.todos[index] = result
 }
 
 // id を指定して Todo を削除する
-const deleteTodo = (id: number) => {
+const deleteTodo = (id: number): void => {
+  TodoRepository.delete(id)
   state.todos = state.todos.filter((todo) => todo.id !== id)
 }
 
 const todoStore: TodoStore = {
   state: readonly(state),
+  fetchTodos,
+  fetchTodo,
   getTodo,
   addTodo,
   updateTodo,
@@ -107,4 +66,4 @@ const todoStore: TodoStore = {
 export default todoStore
 
 // ストアを provide / inject するため､必要なキーを宣言する
-export const todoKey: InjectionKey<TodoStore> = Symbol('todo')
+export const todoKey: InjectionKey<TodoStore> = Symbol('todoKey')
